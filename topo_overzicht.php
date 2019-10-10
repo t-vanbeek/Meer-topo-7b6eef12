a6580239dbf4cc69784e91ca5c4c24c2d37d30d4
Thorvald van Beek <54794887+t-vanbeek@users.noreply.github.com>
2019-10-10 10:33:43 +0200

    Uitwerking van de exercise

--- topo_overzicht.php
+++ topo_overzicht.php
@@ -1,4 +1,25 @@
 <?php
+/*
+Nu je geslaagd bent voor de toets,
+mag je nieuwe hoofdsteden toevoegen aan het programma. 
+Welke landen en hoofdsteden ga jij toevoegen?
 
+Zorg ervoor dat de gebruiker nieuwe landen en hoofdsteden toe kan voegen aan de associative array.
+*/
 
+$landen = array(["Land"],
+                ["Hoofdstad"]);
+
+echo "Hoeveel landen wil je toevoegen?" . PHP_EOL;
+$aantalLanden = readline();
+
+for ($telGetal = 1; $telGetal <= $aantalLanden; $telGetal++) {
+    array_push($landen["Land"] = readline("Welk land wil je toevoegen?" . PHP_EOL));
+    array_push($landen["Hoofdstad"] = readline("Wat is de hoofdstad van " . $landen["Land"] . "?" . PHP_EOL));
+}
+echo "De volgende landen zijn toegevoegd: ".PHP_EOL;
+foreach ($landen as $land){
+    echo $land . PHP_EOL;
+}
+array_
 ?>
